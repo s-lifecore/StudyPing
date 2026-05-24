5d05bc682d97b192f69ba7b7acd0ffe262897336
s-taku0502 <takumi.daimonhigh@gmail.com>
2026-05-25 01:53:40 +0900

    作業開始ボタンを実装

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,10 +1,10 @@
-import { Client, Account, Databases } from "appwrite";
+import { Client, Databases } from "appwrite";
 
 const client = new Client()
   .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
   .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID);
 
-const account = new Account(client);
-const databases = new Databases(client);
+export const databases = new Databases(client);
 
-export { client, account, databases };
+export const DATABASE_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID;
+export const COLLECTION_ID = process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID;
